4bbc172284f119a4f6c429dfe58b556c1a29598f
Jean-Michel Brankart <jean-michel.brankart@universite-grenoble-alpes.fr>
2019-01-12 18:01:08 +0100

    adjust python interface to free format

--- python/signatures/propose_pyf.bash
+++ python/signatures/propose_pyf.bash
@@ -3,6 +3,6 @@
 
 SRCDIR="../../src"
 
-ffile="${SRCDIR}/sesam.F" ; modname='sesam'
+ffile="${SRCDIR}/sesam.F90" ; modname='sesam'
 f2py --overwrite-signature -m ${modname} -h ${modname}.pyf-proposed $ffile
 
